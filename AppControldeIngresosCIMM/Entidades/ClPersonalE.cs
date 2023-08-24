6ee2b583db26d0b37f19c259a978faa3c0d2c54a
Vanessa Hernandez <hdzvanessa122019@hotmail.com>
2023-08-23 20:18:57 -0600

    Cambios

---
+++ AppControldeIngresosCIMM/Entidades/ClPersonalE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppControldeIngresosCIMM.Entidades
+{
+    public class ClPersonalE
+    {
+        public string Documento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Tipo_Personal { get; set; }
+        public string Programa { get; set; }
+        public string Ficha { get; set; }
+        public string Nombre_Articulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Fecha_Hora { get; set; }
+        public string Tipo { get; set; }
+    }
+}
